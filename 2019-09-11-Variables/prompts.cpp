36a3ac3d5cfd6e770bbbc14a4c6d789a9f2b81a5
Ibrahim Torres Mahmud <itorresm@darkstar.org>
2019-09-11 09:01:51 -0500

    Prompts two integers

---
+++ 2019-09-11-Variables/prompts.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(void)
+{
+  int val1 = 0;
+  int val2 = 0;
+  std::cout << "Escriba un entero: " << std::endl;
+  std::cin >> val1;
+  std::cout << "Escriba otro entero: " << std::endl;
+  std::cin >> val2;
+  
+  return 0;
+}
