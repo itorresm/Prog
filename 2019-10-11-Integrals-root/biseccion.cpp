0accb9d8d8c5ea4b86e570c6fe86ae26249a0af0
Ibrahim Torres Mahmud <itorresm@darkstar.org>
2019-10-11 09:02:59 -0500

    Added finding roots

---
+++ 2019-10-11-Integrals-root/biseccion.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+using fptr = double (double);
+
+double fun(double x);
+double bisection(double xl, double xu, double eps, fptr f);
+
+
+int main(void)
+{
+  std::cout.precision(15); std::cout.setf(std::ios::scientific);
+  const double EPS = 1.0e-6;
+  double root = bisection(100, 200, EPS, fun);
+  std::cout << root << "\n";
+  return 0;
+}
+
+double bisection(double xl, double xu, double eps, fptr f)
+{
+  double xr = xl;
+  do
+    {
+      xr =(xl+xu)/2.0;
+      if(std::fabs(f(xr)) <= eps)
+	{
+	  break;
+	}
+      else if (f(xr)*f(xl) < 0)
+	{
+	  xu = xr;
+	}
+      else
+	{
+	  xl = xr;
+	}
+    }
+  while(1);
+  
+  return xr;
+}
+
+double fun(double x)
+{
+  return x+10-x*std::cosh(50.0/x);
+}
