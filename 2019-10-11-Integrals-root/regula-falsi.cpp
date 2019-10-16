aa5cee5e1701b2f7cf01a307d47950d84c2c3916
Ibrahim Torres Mahmud <itorresm@darkstar.org>
2019-10-16 08:56:16 -0500

    Added regula falsi

--- 2019-10-11-Integrals-root/regula-falsi.cpp
+++ 2019-10-11-Integrals-root/regula-falsi.cpp
@@ -5,20 +5,53 @@ using fptr = double (double);
 
 double fun(double x);
 double bisection(double xl, double xu, double eps, fptr f);
-
+double bisection_regulafalsi(double xl, double xu, double eps, fptr f);
 
 int main(void)
 {
   std::cout.precision(15); std::cout.setf(std::ios::scientific);
-  const double EPS = 1.0e-6;
+  const double EPS = 1.0e-8;
   double root = bisection(100, 200, EPS, fun);
-  std::cout << root << "\n";
+  double rootfalsi = bisection_regulafalsi(100, 200, EPS, fun);
+  std::cout << root << "\n" << rootfalsi << "\n";
   return 0;
 }
 
 double bisection(double xl, double xu, double eps, fptr f)
 {
+  int iter = 0;
   double xr = xl;
+  do
+    {
+      xr = (xl+xu)/2.0;
+      if(std::fabs(f(xr)) <= eps)
+	{
+	  break;
+	}
+      else if (f(xr)*f(xl) < 0)
+	{
+	  xu = xr;
+	}
+      else
+	{
+	  xl = xr;
+	}
+      iter++ii;
+    }
+  while(1);
+  std::cout << "# bisection iteration = " << iter << std::endl;
+  
+  return xr;
+}
+
+double fun(double x)
+{
+  return x+10-x*std::cosh(50.0/x);
+}
+
+double bisection_regulafalsi(double xl, double xu, double eps, fptr f)
+{
+   double xr = xl;
   do
     {
       xr = xu- ((f(xu)*(xl-xu))/(f(xl)-f(xu)));
@@ -40,7 +73,3 @@ double bisection(double xl, double xu, double eps, fptr f)
   return xr;
 }
 
-double fun(double x)
-{
-  return x+10-x*std::cosh(50.0/x);
-}
